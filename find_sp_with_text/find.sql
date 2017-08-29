13642d5e06ad605e8e7229328e1db71c3e9b68bb
Nikolai Rovdo <nichogaus@gmail.com>
2017-08-29 12:59:29 +0300

    Update find.sql

--- find_sp_with_text/find.sql
+++ find_sp_with_text/find.sql
@@ -7,3 +7,9 @@ WHERE  Object_definition(object_id) LIKE '%search text%'
 SELECT name
 FROM   sys.objects
 WHERE  Object_definition(object_id) LIKE '%search text%'
+
+-- search by column names
+SELECT c.name AS ColName, t.name AS TableName
+FROM sys.columns c
+    JOIN sys.tables t ON c.object_id = t.object_id
+WHERE c.name LIKE '%postalcode%'
